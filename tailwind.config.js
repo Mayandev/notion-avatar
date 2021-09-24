44f5bee5638bcb4baacbb1bde398621ca326333e
mayandev <phillzou@gmail.com>
2021-09-24 20:38:44 +0800

    chore: remove unused tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,ts,jsx,tsx}', './public/**/*.html'],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
