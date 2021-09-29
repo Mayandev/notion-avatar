534eccc98c4935c6dfbbeecf0ef79d054f3de4cb
mayandev <phillzou@gmail.com>
2021-09-29 22:05:55 +0800

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,32 @@
+import { GetStaticPropsContext } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
+
+import Header from './components/Header';
+
+export default function PageNotFount() {
+  const { t } = useTranslation(`common`);
+
+  return (
+    <div className="h-screen">
+      <Header />
+      <main className="flex flex-col justify-center h-2/3 items-center">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl">{t(`404Title`)}</h1>
+        <Link href="/">
+          <a className="mt-4 underline">← {t(`404Redirect`)}</a>
+        </Link>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps({
+  locale,
+}: GetStaticPropsContext & { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [`common`])),
+    },
+  };
+}
