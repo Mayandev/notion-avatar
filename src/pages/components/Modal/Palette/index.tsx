189b09b3e4954bdfc32c9f2f849666187988d19b
mayandev <phillzou@gmail.com>
2021-10-25 16:45:37 +0800

    fix: palette modal width

--- src/pages/components/Modal/Palette/index.tsx
+++ src/pages/components/Modal/Palette/index.tsx
@@ -39,63 +39,59 @@ export default function PaletteModal({
       }}
     >
       <div className="text-xl bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <section>
-          <h1 className="py-4">{t('chooseShape')}</h1>
-          <div>
+        <h1 className="py-4 w-full">{t('chooseShape')}</h1>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              setBackgroundShape('square');
+            }}
+            className={`bg-gray-400 rounded w-8 h-8 focus:outline-none mr-6 ${
+              bgShape === 'square' && 'ring-2 ring-offset-2 ring-gray-400'
+            }`}
+          >
+            {' '}
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setBackgroundShape('circle');
+            }}
+            className={`bg-gray-400 rounded-full w-8 h-8 focus:outline-none ${
+              bgShape === 'circle' && 'ring-2 ring-offset-2 ring-gray-400'
+            }`}
+          >
+            {' '}
+          </button>
+        </div>
+
+        <h1 className="py-4">{t('paletteTitle')}</h1>
+        <div className="grid gap-2 sm:gap-4 justify-items-center justify-between grid-rows-2 grid-cols-5">
+          {PalettePreset.map((color, index) => (
+            /* eslint-disable */
             <button
+              key={index}
               type="button"
               onClick={() => {
-                setBackgroundShape('square');
+                setColor(color);
               }}
-              className={`bg-gray-400 rounded w-8 h-8 focus:outline-none mr-6 ${
-                bgShape === 'square' && 'ring-2 ring-offset-2 ring-gray-400'
-              }`}
+              className="w-14 h-10 sm:w-20 sm:h-12 rounded focus:ring-2 focus:ring-offset-2"
+              style={{ background: color }}
             >
               {' '}
             </button>
-            <button
-              type="button"
-              onClick={() => {
-                setBackgroundShape('circle');
-              }}
-              className={`bg-gray-400 rounded-full w-8 h-8 focus:outline-none ${
-                bgShape === 'circle' && 'ring-2 ring-offset-2 ring-gray-400'
-              }`}
-            >
-              {' '}
-            </button>
-          </div>
-        </section>
-        <section>
-          <h1 className="py-4">{t('paletteTitle')}</h1>
-          <div className="grid gap-2 sm:gap-4 justify-items-center justify-between grid-rows-2 grid-cols-5">
-            {PalettePreset.map((color, index) => (
-              /* eslint-disable */
-              <button
-                key={index}
-                type="button"
-                onClick={() => {
-                  setColor(color);
-                }}
-                className="w-10 h-7 sm:w-16 sm:h-12 rounded focus:ring-2 focus:ring-offset-2"
-                style={{ background: color }}
-              >
-                {' '}
-              </button>
-            ))}
-          </div>
-        </section>
-        <section className="">
-          <div className="flex items-center">
-            <h1 className="py-4">{t('customColor')}</h1>
-            <input
-              defaultValue={backgroundConfig.color}
-              onChange={handleColorChange}
-              className="ml-2 border-gray-400 border-1 rounded bg-gray-2=100"
-              type="color"
-            ></input>
-          </div>
-        </section>
+          ))}
+        </div>
+
+        <div className="flex items-center">
+          <h1 className="py-4">{t('customColor')}</h1>
+          <input
+            defaultValue={backgroundConfig.color}
+            onChange={handleColorChange}
+            className="ml-2 border-gray-400 border-1 rounded bg-gray-2=100"
+            type="color"
+          ></input>
+        </div>
       </div>
     </Modal>
   );
