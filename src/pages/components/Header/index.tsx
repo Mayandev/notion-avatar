d7b31ab301f2fa648f717fd724c1a4dae099df5a
Mayandev <phillzou@gmail.com>
2025-03-02 14:49:20 +0800

    feat: i18n for title

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/legacy/image';
+import { useTranslation } from 'next-i18next';
 import Decoration from './decoration';
 
 export default function Header() {
+  const { t } = useTranslation('common');
+
   return (
     <header className="relative">
       <div className="flex py-5 px-5 sm:px-16 md:px-32">
@@ -14,7 +17,7 @@ export default function Header() {
         <span className="text-lg">
           Notion
           <br />
-          Avatar Maker
+          {t('avatarMaker')}
         </span>
       </div>
       <Decoration className="absolute top-0 right-0 w-24 sm:w-28 md:w-36" />
